df91cd08cb4c6f7c4a455d5eb045cd70e53681da
AjayT98 <ajay.taak@hotmail.com>
2020-04-17 15:57:44 +0100

    Change Port

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('This is test project!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
